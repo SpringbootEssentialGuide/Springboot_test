27037074ca5cac5e023e6ad4777307c78c703379
Han Jeongwoo <HANJEONGWOO@github.com>
2022-11-25 14:01:10 +0900

    컨트롤러 테스트 추가

--- src/main/java/com/springboot/test/data/dto/ProductDto.java
+++ src/main/java/com/springboot/test/data/dto/ProductDto.java
@@ -1,37 +1,23 @@
 package com.springboot.test.data.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+// 예제 7.9
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class ProductDto {
+
     private String name;
+
     private int price;
+
     private int stock;
 
-    public ProductDto(String name, int price, int stock) {
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
---
+++ src/test/java/com/springboot/test/controller/ProductControllerTest.java
@@ -0,0 +1,88 @@
+package com.springboot.test.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.google.gson.Gson;
+import com.springboot.test.data.dto.ProductDto;
+import com.springboot.test.data.dto.ProductResponseDto;
+import com.springboot.test.service.impl.ProductServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+//일반적으로 WebMvcTest 어노테이션을 사용한 테스트는 슬라이스 테스트라고 부릅니다.
+//컨트롤러는 개념상 웹과 맞닿은 레이어로서 외부 요인을 차단하고 테스트하면 의미가 없기 때문에 슬라이스 테스트를 진행하는 경우가 많습니다.
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+//    MockBean 어노테이션을 통해 ProductController가 의존성을 가지고 있던 ProductService 객체에 Mock 객체를 주입했습니다.
+//    Mock객체에는 테스트 과정에서 맡을 동작을 정의해야 합니다.
+    @MockBean
+    ProductServiceImpl productService;
+
+    @Test
+    @DisplayName("MockMvc를 통한 Product 데이터 가져오기 테스트")
+    void getProductTest() throws Exception {
+
+
+        given(productService.getProduct(123L)).willReturn(
+                new ProductResponseDto(123L, "pen", 5000, 2000));
+
+        String productId = "123";
+
+        mockMvc.perform(
+                get("/product?number=" + productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(
+                        "$.number").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
+
+        verify(productService).getProduct(123L);
+    }
+
+    @Test
+    @DisplayName("Product 데이터 생성 테스트")
+    void createProductTest() throws Exception {
+        //Mock 객체에서 특정 메서드가 실행되는 경우 실제 Return을 줄 수 없기 때문에 아래와 같이 가정 사항을 만들어줌
+        given(productService.saveProduct(new ProductDto("pen", 5000, 2000)))
+                .willReturn(new ProductResponseDto(12315L, "pen", 5000, 20000));
+
+        ProductDto productDto = ProductDto.builder()
+                .name("pen")
+                .price(5000)
+                .stock(2000)
+                .build();
+
+        Gson gson = new Gson();
+        String content = gson.toJson(productDto);
+
+        mockMvc.perform(
+                        post("/product")
+                                .content(content)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
+
+        verify(productService).saveProduct(new ProductDto("pen", 5000, 2000));
+    }
+}
